89d4f1ebf4f41b6a9976118b20a080d10c09cd32
Siddartha <siddartharedddyl@gmail.com>
2024-01-22 12:23:51 +0530

    done editing accout details section:

--- part1/src/Services/service.js
+++ part1/src/Services/service.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import { getAuthToken } from "../util/auth";
 const token = getAuthToken();
 console.log(token);
-const header ={
-    'Authorization': 'Bearer ' + token
-}
+const header = {
+  Authorization: "Bearer " + token,
+};
 const urlSignin = "api/login/";
 const signin = (body) => {
   const promise = axios.post(urlSignin, body);
@@ -15,17 +15,15 @@ const signUp = (body) => {
   const promise = axios.post(urlSignup, body);
   return promise.then((res) => res.data).catch((error) => error);
 };
-const urlEvents = " http://127.0.0.1:8000/api/events/";
+const urlEvents = "http://127.0.0.1:8000/api/events/";
 const getEvents = () => {
   // const promise = axios.get("http://localhost:3001/submit/");
   const promise = axios.get(urlEvents);
-  return promise.then((res) => 
-   
-    res.data).catch((error) => error);
+  return promise.then((res) => res.data).catch((error) => error);
 };
 const fetchEvent = (id) => {
-  const urlEachEvent = urlEvents+id;
-  const promise = axios.get(urlEachEvent,{headers:header});
+  const urlEachEvent = urlEvents + id;
+  const promise = axios.get(urlEachEvent, { headers: header });
   return promise.then((res) => res).catch((error) => error);
 };
 const submitEvent = (body) => {
@@ -38,34 +36,36 @@ const urlEvents2 = "http://127.0.0.1:8000/api/events/self/";
 const userSpecificEvent = () => {
   // const urlFetchEvent = "http://localhost:3001/submit/";
   const urlFetchEvent = urlEvents2;
-  
-  const promise = axios.get(urlFetchEvent,{headers:header});
+
+  const promise = axios.get(urlFetchEvent, { headers: header });
   return promise.then((res) => res).catch((error) => error);
 };
 const fetchParticularEvent = (id) => {
   // const urlFetchParticularEvent = `http://localhost:3001/submit/${id}`;
-  const urlFetchParticularEvent = urlEvents+id;
-  const promise = axios.get(urlFetchParticularEvent,{headers:header});
+  const urlFetchParticularEvent = urlEvents + id;
+  const promise = axios.get(urlFetchParticularEvent, { headers: header });
   return promise.then((res) => res.data).catch((error) => error);
 };
 const editParticularEvent = (id, data) => {
   // const urleditParticularEvent = `http://localhost:3001/submit/${id}`;
-  const urleditParticularEvent =urlEvents+id+"/";
-  
-  const promise = axios.put(urleditParticularEvent, data,{headers:header});
+  const urleditParticularEvent = urlEvents + id + "/";
+
+  const promise = axios.put(urleditParticularEvent, data, { headers: header });
   return promise.then((res) => res.data).catch((error) => error);
 };
-const deleteParticularEvent =(id) =>{
-  // const urldeleteParticularEvent= `http://localhost:3001/submit/${id}`;
-  const urldeleteParticularEvent= urlEvents+id;  
-  const promise = axios.delete(urldeleteParticularEvent,{headers:header});
-  return promise.then((res) => res).catch((error) => error);
-}
 const getEventMessages = (eventId) => {
   // const url = `http://127.0.0.1:8000/api/announcements/`;
   const url = `http://localhost:3001/messages?eventId=${eventId}`;
   return axios.get(url);
 };
+
+const deleteParticularEvent = (id) => {
+  // const urldeleteParticularEvent= `http://localhost:3001/submit/${id}`;
+  const urldeleteParticularEvent = urlEvents + id;
+  const promise = axios.delete(urldeleteParticularEvent, { headers: header });
+  return promise.then((res) => res).catch((error) => error);
+};
+
 const createEventMessage = (eventId, message) => {
   // const url = `http://127.0.0.1:8000/api/announcements/`;
   const url = `http://localhost:3001/messages`;
@@ -74,17 +74,27 @@ const createEventMessage = (eventId, message) => {
   //   broadcast: eventId,
   // };
   const body = {
-    content : message,
+    content: message,
     eventId: eventId,
-    timeStamp: "12:22:00"
-
-  }
+    timeStamp: "12:22:00",
+  };
   return axios.post(url, body);
 };
-const handleDeleteMessage=(eventId)=>{
+const urserAccountUrl = "http://localhost:8000/api/user-data/";
+
+const userAccountDatails = () => {
+  return axios.get(urserAccountUrl, { headers: header });
+};
+const editAccountDetails =(name) =>{
+  const body={
+    name:name
+  }
+  return axios.post(urserAccountUrl,body,{ headers: header })
+}
+const handleDeleteMessage = (eventId) => {
   const url = `http://localhost:3001/messages?eventId=${eventId}`;
   return axios.delete(url);
-}
+};
 export default {
   signin: signin,
   getEvents: getEvents,
@@ -97,5 +107,7 @@ export default {
   getEventMessages,
   createEventMessage,
   deleteParticularEvent,
-  handleDeleteMessage
+  handleDeleteMessage,
+  userAccountDatails,
+  editAccountDetails
 };
--- part1/src/components/AccountDetails.jsx
+++ part1/src/components/AccountDetails.jsx
@@ -1,49 +1,100 @@
 // AccountDetails.js
-import React, { useState } from 'react';
-import { Grid, Typography, Avatar, TextField } from '@mui/material';
-import { deepOrange, deepPurple, teal, amber, pink } from '@mui/material/colors';
-
+import React, { useEffect, useState } from "react";
+import { Grid, Typography, Avatar, TextField, Button } from "@mui/material";
+import {
+  deepOrange,
+  deepPurple,
+  teal,
+  amber,
+  pink,
+} from "@mui/material/colors";
+import api from "../Services/service";
 const getRandomColor = () => {
-  const colors = [deepOrange[500], deepPurple[500], teal[500], amber[500], pink[500]];
+  const colors = [
+    deepOrange[500],
+    deepPurple[500],
+    teal[500],
+    amber[500],
+    pink[500],
+  ];
   const randomIndex = Math.floor(Math.random() * colors.length);
   return colors[randomIndex];
 };
 
 const AccountDetails = () => {
   const userData = {
-    username: 'rohiti',
-    email: 'john.doe@example.com',
-    mobile: '123-456-7890',
+    name: "",
+    email: "",
+    id: 0,
   };
-
-  const [editableUsername, setEditableUsername] = useState(userData.username);
+  const [user, setUser] = useState(userData);
+  useEffect(() => {
+    api.userAccountDatails().then((res) => {
+      console.log(res.data);
+      setUser(res.data);
+    });
+  }, []);
 
   const handleUsernameChange = (event) => {
-    setEditableUsername(event.target.value);
+    console.log(event.target.value);
+    // setUser({ ...event, [event.target.name]: event.target.value });
+    setUser((prevUser) => ({
+      ...prevUser,
+      [event.target.name]: event.target.value,
+    }));
   };
-
+  const handleSubmit=()=>{
+    api.editAccountDetails(user.name).then((res)=>{
+      console.log(res.data.message);
+      alert(res.data.message)
+    }).catch(error =>
+      console.log(error))
+  }
   const avatarColor = getRandomColor();
 
   return (
     <Grid container justifyContent="center" alignItems="center" height="85vh">
       <Grid item xs={12} sm={8} md={6} lg={4}>
         <Grid container direction="column" alignItems="center">
-          <Avatar sx={{ bgcolor: avatarColor, width: 80, height: 80, fontSize: 36, margin: '16px 0' }}>
-            {userData.username.charAt(0)}
+          <Avatar
+            sx={{
+              bgcolor: avatarColor,
+              width: 80,
+              height: 80,
+              fontSize: 36,
+              margin: "16px 0",
+            }}
+          >
+            {/* {user.name.charAt(0)} */}
           </Avatar>
           <Typography variant="h5" gutterBottom>
-            Hello, {userData.username}
+            Hello, {user.name}
           </Typography>
           <TextField
-            label="Username"
+            name="name"
             variant="outlined"
-            value={editableUsername}
+            value={user.name}
             onChange={handleUsernameChange}
             fullWidth
-            sx={{ marginBottom: 2, width: '80%' }}
+            sx={{ marginBottom: 2, width: "80%" }}
           />
-          <TextField label="Email" variant="outlined" value={userData.email} fullWidth disabled sx={{ marginBottom: 2, width: '80%' }} />
-          <TextField label="Mobile Number" variant="outlined" value={userData.mobile} fullWidth disabled sx={{ width: '80%' }} />
+          <TextField
+            label="Email"
+            variant="outlined"
+            value={user.email}
+            fullWidth
+            disabled
+            sx={{ marginBottom: 2, width: "80%" }}
+          />
+          <TextField
+            label="User ID"
+            variant="outlined"
+            value={user.id}
+            fullWidth
+            disabled
+            sx={{ width: "80%" }}
+          />
+          <Button sx={{ paddingTop: 3 }} onClick={handleSubmit} >Save Changes</Button>
         </Grid>
       </Grid>
     </Grid>
@@ -51,5 +102,3 @@ const AccountDetails = () => {
 };
 
 export default AccountDetails;
-
-
