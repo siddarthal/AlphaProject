5c5d75b7744f3a8ab0f0255211cc6e938ce2cc30
Siddartha <siddartharedddyl@gmail.com>
2024-02-07 12:52:57 +0530

    last push edited reset password file loader

--- part1/src/Pages/ResetPassword.jsx
+++ part1/src/Pages/ResetPassword.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
 import axios from "axios";
-import { Link, useNavigate, useNavigation,useParams } from "react-router-dom";
+import { Link, useNavigate, useNavigation, useParams } from "react-router-dom";
 import api from "../Services/service";
+import Loader from "../components/Explore/Loader";
 
 const ResetPassword = () => {
   const navigation = useNavigation();
+  const [loader, setLoader] = useState(false);
   const [formData, setFormData] = useState({
     password: "",
     rePassword: "",
@@ -17,8 +19,8 @@ const ResetPassword = () => {
       [event.target.name]: event.target.value,
     });
   };
-  const {id} =useParams();
-  const {token} =useParams();
+  const { id } = useParams();
+  const { token } = useParams();
   const navigate = useNavigate();
   const isSubmitting = navigation.state === "submitting";
 
@@ -40,106 +42,115 @@ const ResetPassword = () => {
     e.preventDefault();
     if (formValidation()) {
       setErrors({});
+      setLoader(true);
       const { rePassword, ...dataToSend } = formData;
       api
-        .postUpdatePassword(dataToSend,id,token)
+        .postUpdatePassword(dataToSend, id, token)
         .then((result) => {
           if (result.status === 200) {
             alert(`successfully reset password`);
             setFormData({ password: "", rePassword: "" });
             navigate("/signin");
+            setLoader(false);
           }
         })
         .catch((err) => {
           console.error(err);
           alert(`unable to reset password`);
+          setLoader(false);
         });
     } else {
       console.error(`can't validate`);
+      setLoader(false);
     }
   }
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: 400,
-        p: 5,
-        border: "1px solid #ddd",
-        borderRadius: 2,
-        margin: "0 auto",
-        marginTop: 20,
-      }}
-    >
-      <Typography
-        variant="h4"
-        fontWeight="600"
+    <Box>
+      <Box
         sx={{
-          mb: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: 400,
+          p: 5,
+          border: "1px solid #ddd",
+          borderRadius: 2,
+          margin: "0 auto",
+          marginTop: 20,
+          opacity: loader && 0.4,
         }}
       >
-        Reset Password
-      </Typography>
-
-      <TextField
-        label="Password"
-        type="password"
-        placeholder="Enter password"
-        variant="outlined"
-        fullWidth
-        sx={{
-          mb: 3,
-        }}
-        name="password"
-        value={formData.password}
-        onChange={handleInputChange}
-      />
-      {errors.password && (
-        <Typography variant="body2" fontWeight="600">
-          {errors.password}
+        <Typography
+          variant="h4"
+          fontWeight="600"
+          sx={{
+            // mb: 1,
+            pb: 5,
+          }}
+        >
+          Reset Password
         </Typography>
-      )}
-      <TextField
-        label="Password Again"
-        type="password"
-        placeholder="re-enter password"
-        variant="outlined"
-        fullWidth
-        sx={{
-          mb: 3,
-        }}
-        name="rePassword"
-        value={formData.rePassword}
-        onChange={handleInputChange}
-      />
-      <>
-        {errors.rePassword && (
-          <Typography
-            sx={{ marginBottom: "10px" }}
-            variant="body2"
-            fontWeight="600"
-            className="flex-start"
-          >
-            {errors.rePassword}
+
+        <TextField
+          label="Password"
+          type="password"
+          placeholder="Enter password"
+          variant="outlined"
+          fullWidth
+          sx={{
+            mb: 3,
+          }}
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+        {errors.password && (
+          <Typography variant="body2" fontWeight="600">
+            {errors.password}
           </Typography>
         )}
-      </>
-      <Button
-        variant="contained"
-        disableElevation
-        sx={{
-          py: 1.5,
-          fontWeight: 500,
-          fontSize: 16,
-          width: 150,
-        }}
-        disabled={isSubmitting}
-        onClick={signupHandler}
-      >
-        {isSubmitting ? "signing u up..." : "Reset "}
-      </Button>
+        <TextField
+          label="Password Again"
+          type="password"
+          placeholder="re-enter password"
+          variant="outlined"
+          fullWidth
+          sx={{
+            mb: 3,
+          }}
+          name="rePassword"
+          value={formData.rePassword}
+          onChange={handleInputChange}
+        />
+        <>
+          {errors.rePassword && (
+            <Typography
+              sx={{ marginBottom: "10px" }}
+              variant="body2"
+              fontWeight="600"
+              className="flex-start"
+            >
+              {errors.rePassword}
+            </Typography>
+          )}
+        </>
+        <Button
+          variant="contained"
+          disableElevation
+          sx={{
+            // py: 1.5,
+            fontWeight: 500,
+            fontSize: 16,
+            width: 150,
+          }}
+          disabled={loader}
+          onClick={signupHandler}
+        >
+          {isSubmitting ? "signing u up..." : "Reset "}
+        </Button>
+      </Box>
+      {loader && <Loader />}
     </Box>
   );
 };
